b68f51bc5dd5b97bd5f2ebb330a7bae2b974a29b
wsyxka <wsxkeay@outlook>
2021-07-06 16:09:41 +0800

    exam

---
+++ shopcart/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="content">
+    <router-view />
+    </div> 
+    <div class="bottom">
+      <router-link to="/" tag="div">商品列表</router-link>
+      <router-link to="/shopcart" tag="div">购物车</router-link>
+    </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
